dfd0c40c99c402571e846334aa0ccb6c7203b316
Iwan BK <iwanbk@gmail.com>
2024-04-25 11:52:10 +0700

    initial ios work

--- ios/Classes/TunFlutterPlugin.swift
+++ ios/Classes/TunFlutterPlugin.swift
@@ -2,18 +2,24 @@ import Flutter
 import UIKit
 
 public class TunFlutterPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "tun_flutter", binaryMessenger: registrar.messenger())
-    let instance = TunFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "tun_flutter", binaryMessenger: registrar.messenger())
+        let instance = TunFlutterPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        case "getBatteryLevel":
+            result(90)
+        case "startVpn":
+            result(true)
+        case "getTunFD":
+            result(1)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
